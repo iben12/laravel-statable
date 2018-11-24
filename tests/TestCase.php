0a54ced0459d49fc7aaf91509052878f1ddcc942
iben12 <iben@iben.hu>
2018-11-24 13:35:40 +0100

    Fix migration path in test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -73,7 +73,7 @@ abstract class TestCase extends BaseTestCase
             $table->timestamps();
         });
 
-        include_once __DIR__.'/../database/migrations/create_state_history_table.php.stub';
+        include_once __DIR__.'/../database/migrations/create_state_history_table.php';
 
         (new \CreateStateHistoryTable())->up();
 
