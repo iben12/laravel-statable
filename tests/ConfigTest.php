031b431bba58a440cd098801eabbeab61e59f0af
Vincent Prat <vpratfr@gmail.com>
2020-04-30 10:56:33 +0200

    Allow using a custom StateHistory model

---
+++ tests/ConfigTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Iben\Statable\Test;
+
+use Iben\Statable\Models\StateHistory;
+use Iben\Statable\Services\StateHistoryManager;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
+use Sebdesign\SM\Event\TransitionEvent;
+use SM\Event\SMEvents;
+use SM\StateMachine\StateMachine;
+
+class CustomStateHistory extends StateHistory {}
+
+class ConfigTest extends TestCase
+{
+    /** @var StatableArticle */
+    public $article;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->app->bind(StateHistoryManager::class);
+        $this->app['config']->set('state-machine.article.class', StatableArticle::class);
+        $this->app['config']->set('laravel-statable.models.state_history', CustomStateHistory::class);
+
+        $this->article = StatableArticle::firstOrCreate([
+            'title' => 'Test Article',
+            'state' => 'new',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_use_custom_state_history_model()
+    {
+        $this->article->apply('create');
+        $this->assertInstanceOf(CustomStateHistory::class, $this->article->stateHistory()->first());
+    }
+
+}
